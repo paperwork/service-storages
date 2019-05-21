5f573085c8e05dcbd3e2d2878da0d8dce539c5ba
Marius <marius@twostairs.com>
2019-05-21 22:06:26 +0200

    Generic application.ex

---
+++ lib/application.ex
@@ -0,0 +1,21 @@
+defmodule Paperwork.Storages.Application do
+    use Application
+
+    def start(_type, _args) do
+        case Code.ensure_loaded(ExSync) do
+            {:module, ExSync = mod} ->
+                mod.start()
+            {:error, _} ->
+                :ok
+        end
+
+        children = [
+            Paperwork.Ex,
+            Paperwork.Storages.Server,
+            {Mongo, [name: :mongo, url: Confex.fetch_env!(:paperwork, :mongodb)[:url], pool: DBConnection.Poolboy]}
+        ]
+
+        opts = [strategy: :one_for_one, name: Paperwork.Storages.Supervisor]
+        Supervisor.start_link(children, opts)
+    end
+end
